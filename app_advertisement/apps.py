44d158cca4792a7078537593fc49fcd149eb0ef6
drimrex <144201537+drimrex@users.noreply.github.com>
2023-10-15 11:10:02 +0300

    Update apps.py

--- app_advertisement/apps.py
+++ app_advertisement/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class AppAdvertisementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_advertisement'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = 'app_advertisements'
+    verbose_name = 'Объявления'
