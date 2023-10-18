c9500ae4155d1391197902f5338ed02f3bd1972e
drimrex <144201537+drimrex@users.noreply.github.com>
2023-10-18 18:16:50 +0300

    Update models.py

--- app_advertisement/models.py
+++ app_advertisement/models.py
@@ -1,8 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
+
 # Create your models here.
-# Заголовок, цена, описание, дата создания, дата обновления, уместен ли торг 
+# Заголовок, цена, описание, from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Advertisement(models.Model):
     # СharField - строковое поле для небольших строк 
@@ -17,6 +21,8 @@ class Advertisement(models.Model):
     # DateTimeField - cпециальное поле для работы с датами 
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
     updated_at = models.DateTimeField('Дата обновления', auto_now=True)
+    user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE, blank=True,null=True)
+    image = models.ImageField("изображение", upload_to="advertisements/")
   
     @admin.display(description='Дата создания')
     def created_date(self):
@@ -28,15 +34,16 @@ class Advertisement(models.Model):
             )
         return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
     
-    @admin.display(description='дата последнего обновления')
+    @admin.display(description='Дата создания')
     def updated_date(self):
         from django.utils import timezone
-        if self.updated_at.date() == timezone.now().date():
-            created_time = self.updated_at.time().strftime("%H:%M:%S")
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
             return format_html(
-                '<span style="color: red; font-weight: bold;">Сегодня в {}</span>', created_time
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
             )
-        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+    
     
     def __str__(self):
         return f'Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})'
