b922c7c06d9ca85a2cf7ef2de4b0d081e4035b23
drimrex <144201537+drimrex@users.noreply.github.com>
2023-10-22 11:12:26 +0300

    Update views.py

--- app_advertisement/views.py
+++ app_advertisement/views.py
@@ -1,9 +1,28 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .models import Advertisement
+from .forms import AdvertisementForm
 
-def index(reqest):
-    return render(reqest,'index.html')
+def index(request):
+    advertisements = Advertisement.objects.all()
+    context = {'advertisements': advertisements}
+    return render(request, 'index.html', context)
+    
 # Create your views here.
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
 
-def top_sellers(reqest):
-    return render(reqest,'top-sellers.html')
+def advertisement_post(request):
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_advertisement = form.save(commit=False)
+            new_advertisement.user = request.user
+            new_advertisement.save()
+            url = reverse('main_page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
+    context = {'form': form}
+    return render(request, 'advertisement-post.html', context)
